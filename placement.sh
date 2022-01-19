c9b8a62bd6277472406050ec0451b5eba1185158
shhan0226 <57717799+shhan0226@users.noreply.github.com>
2022-01-20 00:18:27 +0900

    Update placement.sh

--- placement.sh
+++ placement.sh
@@ -75,6 +75,6 @@ echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
 echo "osc ... "
 . admin-openrc
 
-pip install osc-placement
+pip3 install osc-placement
 openstack --os-placement-api-version 1.2 resource class list --sort-column name
 openstack --os-placement-api-version 1.6 trait list --sort-column name
